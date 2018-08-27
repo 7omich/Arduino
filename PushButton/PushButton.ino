c6f37a8c3971c69f85298ec741e1579e5829cfc1
nb6u7 <b6u728@gmail.com>
2018-08-27 12:38:30 +0900

    Impl old_val

--- PushButton/PushButton.ino
+++ PushButton/PushButton.ino
@@ -2,6 +2,7 @@ const int LED = 13; // LEDが接続されているピン
 const int BUTTON = 7; // プッシュボタンが接続されているピン
 
 int val = 0; // 入力ピンの状態を記憶する変数
+int old_val = 0; // valの前の値を保存しておく変数
 int state = 0; // LEDの状態(0 or 1)
 
 void setup() {
@@ -12,9 +13,11 @@ void setup() {
 void loop() {
   val = digitalRead(BUTTON); // 入力を読み取り、valに格納
 
-  if (val == HIGH) {
+  if ((val == HIGH) && (old_val == LOW)) {
     state = 1 - state;
   }
+
+  old_val = val;
 
   if (state == 1) {
     digitalWrite(LED, HIGH); // LEDをオンにする
