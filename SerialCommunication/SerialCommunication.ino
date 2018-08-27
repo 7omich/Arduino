a25bb076793cd694ec4298115e8158724caf313e
nb6u7 <b6u728@gmail.com>
2018-08-27 21:14:40 +0900

    Update to light LED

--- SerialCommunication/SerialCommunication.ino
+++ SerialCommunication/SerialCommunication.ino
@@ -1,7 +1,11 @@
 const int SENSOR = 0;
+const int LED = 9;
+
 int val = 0;
+int brightness = 0;
 
 void setup() {
+  pinMode(LED, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -9,5 +13,15 @@ void loop() {
   val = analogRead(SENSOR);
 
   Serial.println(val);
-  delay(100);
+
+  if (val < 600) {
+    brightness = 0;
+  } else if (val > 855) {
+    brightness = 255;
+  } else {
+    brightness = val - 600;
+  }
+  
+  analogWrite(LED, brightness);
+  delay(50);
 }
