b0931f43dd90dbbd6c81acce994157d6c5baa3fa
nb6u7 <b6u728@gmail.com>
2018-08-27 12:28:21 +0900

    Add state variable

--- PushButton/PushButton.ino
+++ PushButton/PushButton.ino
@@ -2,6 +2,7 @@ const int LED = 13; // LEDが接続されているピン
 const int BUTTON = 7; // プッシュボタンが接続されているピン
 
 int val = 0; // 入力ピンの状態を記憶する変数
+int state = 0; // LEDの状態(0 or 1)
 
 void setup() {
   pinMode(LED, OUTPUT); // ArduinoにLEDが出力であると伝える
@@ -12,6 +13,10 @@ void loop() {
   val = digitalRead(BUTTON); // 入力を読み取り、valに格納
 
   if (val == HIGH) {
+    state = 1 - state;
+  }
+
+  if (state == 1) {
     digitalWrite(LED, HIGH); // LEDをオンにする
   } else {
     digitalWrite(LED, LOW); // LEDをオフにする
